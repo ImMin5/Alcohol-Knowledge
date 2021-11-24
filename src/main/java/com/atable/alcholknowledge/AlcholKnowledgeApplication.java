a0a518840b887145ffc0d03ddc09eb25611687e9
16junys <popo3176junys@gmail.com>
2021-11-24 15:05:37 +0900

    [FEAT] Create new spring boot project

---
+++ src/main/java/com/atable/alcholknowledge/AlcholKnowledgeApplication.java
@@ -0,0 +1,13 @@
+package com.atable.alcholknowledge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlcholKnowledgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlcholKnowledgeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/atable/alcholknowledge/AlcholKnowledgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atable.alcholknowledge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlcholKnowledgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

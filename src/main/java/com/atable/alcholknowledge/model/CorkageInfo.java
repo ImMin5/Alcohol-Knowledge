3113dea6f650bf8934dff40d423baa7d48ee3672
16junys <popo3176junys@gmail.com>
2022-01-23 23:12:01 +0900

    [FIX] Fix corkage store, info id generating strategy - corkageStore, corkageInfo id 값이 1씩 증가하지 않는 문제 수정

--- src/main/java/com/atable/alcholknowledge/model/CorkageInfo.java
+++ src/main/java/com/atable/alcholknowledge/model/CorkageInfo.java
@@ -18,7 +18,7 @@ import java.util.Date;
 public class CorkageInfo {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
     @Column(name = "description")
